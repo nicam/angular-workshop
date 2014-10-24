71a8989f300cc20ea22485ced8cce63850412387
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2014-10-24 14:28:38 +0200

    more advanced

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-var app = angular.module('store', []);
+var app = angular.module('store', ['ui.router']);
 
-app.controller('StoreController', function () {
-  console.log('blaa');
-  this.products = [
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/products");
+
+  $stateProvider
+    .state('products', {
+      url: "/products",
+      templateUrl: "partials/store.html",
+      controller: 'StoreController',
+    })
+    .state('checkout', {
+      url: "/checkout",
+      templateUrl: "partials/checkout.html",
+      controller: 'CheckoutController',
+    });
+});
+
+app.controller('NavigationController', function ($scope, $rootScope, cartService) {
+  $rootScope.$on('cartUpdate', updateCount);
+  updateCount();
+
+  function updateCount () {
+    $scope.count = cartService.getSize();
+  }
+});
+
+app.controller('StoreController', function ($scope, $rootScope, cartService) {
+  $scope.products = [
     {
       name: "Dodecahedron", 
       price: 2.95, 
@@ -19,8 +43,47 @@ app.controller('StoreController', function () {
     }
   ];
 
-  this.addToCart = function (product) {
+  $scope.addToCart = function (product) {
+    cartService.addItem(product);
+  };
 
+});
+
+app.filter('total', function () {
+  return function (input) {
+    var total = 0;
+    angular.forEach(input, function (v, k) {
+      total += v.price;
+    });
+    return total;
   }
+});
 
+app.controller('CheckoutController', function ($scope, cartService) {
+  $scope.getItems = cartService.getItems();
+
+  $scope.removeFromCart = function (index) {
+    cartService.removeItem(index);
+  };
+});
+
+app.service('cartService', function($rootScope) {
+  var cart = [];
+  this.addItem = function(item) {
+    cart.push(item);
+    $rootScope.$emit('cartUpdate');
+  };
+
+  this.getItems = function () {
+    return cart;
+  };
+
+  this.removeItem = function (index) {
+    cart.splice(index, 1);
+    $rootScope.$emit('cartUpdate');
+  };
+
+  this.getSize = function () {
+    return cart.length;
+  };
 });
